3af11409810b1203ffc43188c950714b4446f8db
Kirill Kostakov <kr1sel@Kr1sels-MacBook-Pro.local>
2022-04-17 16:25:45 +0300

    raid stats fix

--- data.py
+++ data.py
@@ -12,13 +12,13 @@ raids = {
     '\U0001F98B Vow of the Disciple': '2168422218',
     '\U0001F48E Vault of Glass': '3114569402',
     '\U0001F5FF Deep Stone Crypt': '3185876102',
-    '\U0000269C Garden of Salvation':  '3804486505',
+    '\U0001F313 Garden of Salvation':  '3804486505',
     '\U0001F30C Last Wish': '3448775736',
-    '\U0000FE0FCrown of Sorrow': '3292013047',
-    'Scourge of the Past': '1455741693',
-    'Spire of Stars': '3996781284',
-    'Eater of Worlds': '1627755918',
-    'Leviathan': '2266286943'
+    '\U0000269C Crown of Sorrow': '3292013047',
+    '\U0000269C Scourge of the Past': '1455741693',
+    '\U0000269C Spire of Stars': '3996781284',
+    '\U0000269C Eater of Worlds': '1627755918',
+    '\U0000269C Leviathan': '2266286943'
 }
 
 
