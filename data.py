ceeb9d42ee21ac24073ebcfcfa989034a5aaa30e
Kirill Kostakov <kr1sel@Kr1sels-MacBook-Pro.local>
2022-04-17 16:17:02 +0300

    raid stats fix

--- data.py
+++ data.py
@@ -9,12 +9,12 @@ classes = {
 
 raids = {
 
-    'Vow of the Disciple': '2168422218',
-    'Vault of Glass': '3114569402',
-    'Deep Stone Crypt': '3185876102',
-    'Garden of Salvation':  '3804486505',
-    'Last Wish': '3448775736',
-    'Crown of Sorrow': '3292013047',
+    '\U0001F98B Vow of the Disciple': '2168422218',
+    '\U0001F48E Vault of Glass': '3114569402',
+    '\U0001F5FF Deep Stone Crypt': '3185876102',
+    '\U0000269C Garden of Salvation':  '3804486505',
+    '\U0001F30C Last Wish': '3448775736',
+    '\U0000FE0FCrown of Sorrow': '3292013047',
     'Scourge of the Past': '1455741693',
     'Spire of Stars': '3996781284',
     'Eater of Worlds': '1627755918',
