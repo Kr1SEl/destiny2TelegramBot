73e8a29eb10c87b350dd7cd813e7c8e64ae2f2b1
Kirill Kostakov <kr1sel@Kr1sels-MacBook-Pro.local>
2022-04-20 21:44:53 +0300

    whereIsXur fix

--- startBot.py
+++ startBot.py
@@ -164,11 +164,12 @@ def whereIsXur(update: Update, context: CallbackContext):
 
 # using UTC - my time - 3h
 def xurNotifier(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text='Xûr notifier was succesfully set!\U00002604\nYou are gonna receive notification about his location every time he appears in the game. Stay safe, Guardian!')
     timeToNitify = datetime.time(
         hour=18, minute=32, second=00, tzinfo=pytz.UTC)
-    context.job_queue.run_daily(whereIsXur, context=update.effective_chat.id, days=(
+    context.job_queue.run_daily(whereIsXur, context=chat_id, days=(
         0, 1, 2, 3, 4, 5, 6), time=timeToNitify)
 
 
